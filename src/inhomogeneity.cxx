aa830c991cf2a1baa79b2042d09474edcbf8a01e
Soheil <Damangir@users.noreply.github.com>
2014-07-19 00:37:47 +0200

    Modify N4 to accept external mask

--- src/inhomogeneity.cxx
+++ src/inhomogeneity.cxx
@@ -28,13 +28,14 @@ int main(int argc, char *argv[])
   {
     std::cerr << "Missing Parameters " << std::endl;
     std::cerr << "Usage: " << argv[0];
-    std::cerr << " input output";
+    std::cerr << " input mask output";
     std::cerr << std::endl;
     return EXIT_FAILURE;
   }
 
-  std::string InputImage(argv[1]);
-  std::string outputImage(argv[2]);
+  std::string inputImage(argv[1]);
+  std::string maskImage(argv[2]);
+  std::string outputImage(argv[3]);
 
   const unsigned int ImageDimension = 3;
   typedef float PixelType;
@@ -45,7 +46,8 @@ int main(int argc, char *argv[])
 
   typedef itk::N4ImageFilter<ImageType> N4CorrectorType;
   N4CorrectorType::Pointer n4Corrector = N4CorrectorType::New();
-  n4Corrector->SetInput(CU::LoadImage<ImageType>(InputImage));
+  n4Corrector->SetInput(0,CU::LoadImage<ImageType>(inputImage));
+  n4Corrector->SetInput(1,CU::LoadImage<ImageType>(maskImage));
   CU::WriteImage<ImageType>(outputImage, n4Corrector->GetOutput());
 
   return EXIT_SUCCESS;
