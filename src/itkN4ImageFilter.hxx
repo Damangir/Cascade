0e079e5c688422f733f6bb505e1d41ddedbdcdb6
soheil <soheil@soheils-mbp.karolinska.se>
2014-07-20 03:19:46 +0200

    Copyright notice

--- src/itkN4ImageFilter.hxx
+++ src/itkN4ImageFilter.hxx
@@ -1,19 +1,4 @@
-/*
- * Copyright (C) 2013 Soheil Damangir - All Rights Reserved
- * You may use and distribute, but not modify this code under the terms of the
- * Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License
- * under the following conditions:
- *
- * Attribution: You must attribute the work in the manner specified by the
- * author or licensor (but not in any way that suggests that they endorse you
- * or your use of the work).
- * Noncommercial: You may not use this work for commercial purposes.
- * No Derivative Works: You may not alter, transform, or build upon this
- * work
- *
- * To view a copy of the license, visit
- * http://creativecommons.org/licenses/by-nc-nd/3.0/
- */
+/* Copyright (C) 2013-2014 Soheil Damangir - All Rights Reserved */
 #ifndef __itkN4ImageFilter_hxx
 #define __itkN4ImageFilter_hxx
 #include "itkN4ImageFilter.h"
