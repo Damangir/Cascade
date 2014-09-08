e5252f8e3a3bcf9bd2b14de2956c550e798dd425
Soheil <Damangir@users.noreply.github.com>
2014-09-09 00:00:45 +0200

    Calculate volume per label in atlas

---
+++ src/atlas.cxx
@@ -0,0 +1,75 @@
+/* Copyright (C) 2013-2014 Soheil Damangir - All Rights Reserved */
+
+#include "itkImageRegionConstIterator.h"
+#include "imageHelpers.h"
+#include "map"
+
+namespace CU = cascade::util;
+
+int main(int argc, char *argv[])
+{
+  if (argc < 4)
+  {
+    std::cerr << "Missing Parameters " << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " segmentation atlas [thresh=0]";
+    std::cerr << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::string segmentation(argv[1]);
+  std::string atlas(argv[2]);
+  float thresh = 0;
+  if (argc > 3)
+  {
+    thresh = atof(argv[3]);
+  }
+
+  const unsigned int ImageDimension = 3;
+  typedef unsigned int PixelType;
+  typedef double CoordinateRepType;
+  const unsigned int SpaceDimension = ImageDimension;
+
+  typedef std::map< PixelType, std::size_t > AtlasMapType;
+
+  typedef itk::Image< PixelType, ImageDimension > ImageType;
+  typedef itk::ImageRegionConstIterator< ImageType > ImageIteratorType;
+
+  ImageType::Pointer segImg = CU::LoadImage< ImageType >(segmentation);
+  ImageType::Pointer atlasImg = CU::LoadImage< ImageType >(atlas);
+
+  float voxelSize = 1;
+  for(unsigned int i=0;i<ImageDimension;i++)
+  {
+    voxelSize*= segImg->GetSpacing()[i];
+  }
+
+
+  ImageIteratorType segIt(segImg, segImg->GetLargestPossibleRegion());
+  ImageIteratorType atlasIt(atlasImg, atlasImg->GetLargestPossibleRegion());
+
+  AtlasMapType atlasMap;
+
+  while (!segIt.IsAtEnd())
+  {
+    PixelType seg = segIt.Get();
+    PixelType atl = atlasIt.Get();
+    if (seg > thresh)
+    {
+      atlasMap[atl] += 1;
+    }
+    else
+    {
+      atlasMap[atl] += 0;
+    }
+    ++segIt;
+    ++atlasIt;
+  }
+
+  for (AtlasMapType::const_iterator it = atlasMap.begin(); it != atlasMap.end(); ++it)
+  {
+    std::cout << it->first << ":" << it->second * voxelSize<< std::endl;
+  }
+
+  return EXIT_SUCCESS;
+}
