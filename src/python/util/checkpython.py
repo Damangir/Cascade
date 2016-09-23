63a415405ab102cb3fb4b0ef7dc307fc3e162da7
Soheil <Damangir@users.noreply.github.com>
2016-09-23 20:31:11 +0200

    Remove depreciated python interface

--- src/python/util/checkpython.py
+++
@@ -1,5 +0,0 @@
-import sys
-if sys.version_info<(2,7,0):
-    sys.stderr.write("You need python 2.7 or later to run the Cascade.\n")
-    sys.stderr.write("You are using python"+sys.version+".\n")
-    exit(1)
